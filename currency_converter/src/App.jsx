2209a29efd533e046c8ee37b2b1912998bc1d8e9
Nana Kwame <owusugabriel803@gmail.com>
2025-12-10 23:53:29 +0000

    Added the Amount and CurrecnySymbol components

--- currency_converter/src/App.jsx
+++ currency_converter/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 
 import Currency_background from "./assets/Currency_background.jpg";
 import Header from "./Components/header";
+import CurrencySymbol from "./components/CurrencySymbol";
+import Amount from "./components/Amount";
 
 function App() {
   return (
@@ -11,6 +13,10 @@ function App() {
         style={{ "--bg-image": `url(${Currency_background})` }}
       >
         <Header />
+        <div class="bg-white shadow-2xl h-70 w-200 mt-30 ml-125 flex flex-col items-center justify-center space-y-5">
+          <CurrencySymbol />
+          <Amount />
+        </div>
       </div>
     </>
   );
---
+++ currency_converter/src/components/CurrencySymbol.jsx
@@ -0,0 +1,16 @@
+export default function CurrencySymbol() {
+  return (
+    <div>
+      <input
+        class="rounded-2xl p-5 bg-white border-b hover:border outline-none shadow-[0_-4px_9px_rgba(0,0,0,0.2)] h-15 w-75 mr-10 text-black"
+        type="text"
+        placeholder="Enter text here"
+      />
+      <input
+        class="rounded-2xl p-5 bg-white border-b hover:border outline-none shadow-[0_-4px_9px_rgba(0,0,0,0.2)] h-15 w-75 text-black"
+        type="text"
+        placeholder="Enter text here"
+      />
+    </div>
+  );
+}

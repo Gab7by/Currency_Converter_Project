720c599f0ee6d9b81574fdd3f95ab7c0c42271fc
Nana Kwame <owusugabriel803@gmail.com>
2025-12-10 16:22:12 +0000

    Added the Header component and a background image

--- currency_converter/src/App.jsx
+++ currency_converter/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
 
+import Currency_background from "./assets/Currency_background.jpg";
+import Header from "./Components/header";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div
+        className="app-background"
+        style={{ "--bg-image": `url(${Currency_background})` }}
+      >
+        <Header />
+      </div>
+    </>
+  );
 }
 
 export default App;
